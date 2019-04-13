8f3940baf45bbcb10826977d6f3589082a9f9d9c
quannk <quannk@coccoc.com>
2019-04-13 21:50:22 +0700

    fix - one document can be duplicated in search result

--- src/main/java/quannk/test/se/search/InvertedIndexSearcher.java
+++ src/main/java/quannk/test/se/search/InvertedIndexSearcher.java
@@ -6,9 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import quannk.test.se.index.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class InvertedIndexSearcher implements Searcher {
@@ -38,12 +36,16 @@ public class InvertedIndexSearcher implements Searcher {
 				return Collections.emptyList();
 			}
 			List<Pair<Score, Document>> result = new ArrayList<>();
+			Set<Document> documentSet = new HashSet<>();
 			for (TermId termId : query.getTerms()) {
 				for (DocId docId : invertedIndexer.getDocumentsContainingTerm(termId)) {
 					Document document = documentsStorage.getDoc(docId);
-					final Score score = ranker.score(document, query);
-					result.add(Pair.with(score, document));
+					documentSet.add(document);
 				}
+			}
+			for (Document document : documentSet) {
+				final Score score = ranker.score(document, query);
+				result.add(Pair.with(score, document));
 			}
 			// compare by score, the bigger come first
 			result.sort((o1, o2) -> o2.getValue0().compareTo(o1.getValue0()));
